1d7765b61172cdbc8f63e74e8c665fc44ec14ccf
gratipine <gratipine@protonmail.com>
2020-09-23 22:41:41 +0100

    adding ticket types to app

--- visualization/app.py
+++ visualization/app.py
@@ -9,20 +9,27 @@ import numpy as np
 st.title("From A to B")
 data = dp.read_data()
 
-st.dataframe(data.head())
+
+#st.dataframe(data[data["description_ticket"] == ticket_type_required].head())
 destinations = np.sort(data["description"].unique())
 origins = np.sort(data["origin_station"].unique())
 
-origin_station = st.selectbox("Origin station: ", origins)
+origin_station = st.sidebar.selectbox("Origin station: ", origins)
 
-destination_station = st.selectbox("Destination station: ", destinations)
-
-ticket_types = np.sort(data["ticket_code"].unique())
-ticket_type = st.selectbox("Ticket type: ", ticket_types)
+destination_station = st.sidebar.selectbox("Destination station: ", destinations)
 
 price = data.loc[
-    (data["origin_station"] == origin_station) &
+    #(data["origin_station"] == origin_station) &
+    #(data["description_ticket"] == ticket_type_required) &
     (data["description"] == destination_station), :]
+
+ticket_types = list(price["description_ticket"].unique())
+
+ticket_type_required = st.sidebar.selectbox("Ticket type: ", ticket_types, 0)
+
+price = price[(price["description_ticket"] == ticket_type_required)]
 
 # TODO: Need to have a look at the ticket types
 st.dataframe(price)
+
+st.dataframe(data[data["description"] == "HASLEMERE"])
