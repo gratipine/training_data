8b2e805192dc01daa33dd02f5bc736df7f2519fd
gratipine <gratipine@protonmail.com>
2021-05-08 20:43:27 +0100

    Functionalizing reading in of data

--- src/faremap_data_prep.py
+++ src/faremap_data_prep.py
@@ -17,3 +17,28 @@ def line_to_dt(dictionary_with_lines):
     out = pd.DataFrame(dictionary_with_lines.get("stationIds"), columns=["station_ids"])
     out["branch_name"] = dictionary_with_lines.get("branchName")
     return out
+
+def json_prices_to_dt(input_json, stations):
+    station_id = input_json.get("fromId")
+    out = pd.DataFrame()
+    for key in sorted(input_json.get("fares").keys()):
+        dictionary_of_station = input_json.get("fares").get(key)
+
+        for ticket in dictionary_of_station:
+            if ticket.get("ticketName") == 'SDS ANYTIME DAY S   ':
+                temp = pd.DataFrame({
+                    "station_id": key,
+                    "price": ticket.get("price"),
+                    "isDefaultRoute": ticket.get("isDefaultRoute"),
+                    "routeDescription": ticket.get("routeDescription"),
+                    "offPeakOnly": ticket.get("offPeakOnly")
+                }, index=[0])
+
+                out = pd.concat([temp, out])
+
+    explanatory_table = out.merge(stations)
+    explanatory_table["from_station_id"] = station_id
+    explanatory_table["from_station_name"] = str(stations.loc[
+        stations["station_id"] == station_id, "station_name"].values[0])
+
+    return explanatory_table
--- tests/test_faremap_data_prep.py
+++ tests/test_faremap_data_prep.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import pandas as pd
+import copy
 
 import src.faremap_data_prep as fdp
 
@@ -19,3 +20,63 @@ class FaremapDataPrep(TestCase):
         result = fdp.line_to_dt(dictionary_with_lines=line_dictionary)
         result.sort_index(axis=1, inplace=True)
         pd.testing.assert_frame_equal(result, expected)
+    
+    def test_json_prices_to_dt(self):
+        input_station_id = "dummy_id"
+        dest_station_1_list = [
+            {'price': 2710,
+             'offPeakOnly': False,
+             'ticketName': 'SDS ANYTIME DAY S   ',
+             'isDefaultRoute': True,
+             'isTFL': False,
+             'routeDescription': 'ANY PERMITTED'},
+            {'price': 2090,
+             'offPeakOnly': False,
+             'ticketName': 'SDS ANYTIME DAY S   ',
+             'isDefaultRoute': True,
+             'isTFL': False,
+             'routeDescription': 'VIA NARNIA'},
+             {'hops': [],
+              'price': 1840,
+              'offPeakOnly': False,
+              'ticketName': 'Split Ticket',
+              'isDefaultRoute': False,
+              'isTFL': False,
+              'routeDescription': 'Via countryside'},
+        ]
+
+        dest_2_list = copy.deepcopy(dest_station_1_list[0:2])
+        dest_2_list[0]["price"] = 1900
+
+        input_json = {
+            "fromId": input_station_id, 
+            "fares": {
+                "dest_station_1": dest_station_1_list,
+                "dest_station_2": dest_2_list
+            }
+
+        }
+        stations = pd.DataFrame({
+            "station_id": 
+                ["dest_station_1", "dest_station_2", input_station_id],
+            "station_name": 
+                ["First Station", "Second Station", "Origin"]
+        })
+        result = fdp.json_prices_to_dt(input_json, stations)
+        expected = pd.DataFrame([
+            ["dest_station_1", 2710, True, "ANY PERMITTED", False, 
+            "First Station", input_station_id, "Origin"],
+            ["dest_station_1", 2090, True, 'VIA NARNIA', False, 
+            "First Station", input_station_id, "Origin"],
+            ["dest_station_2", 1900, True, "ANY PERMITTED", False, 
+            "Second Station", input_station_id, "Origin"],
+            ["dest_station_2", 2090, True, 'VIA NARNIA', False, 
+            "Second Station", input_station_id, "Origin"],
+        ], columns=[
+            "station_id", "price", "isDefaultRoute", 
+            "routeDescription", "offPeakOnly", "station_name", 
+            "from_station_id", "from_station_name"])
+        print(expected)
+        print(result)
+        pd.testing.assert_frame_equal(
+            expected, result.iloc[::-1].reset_index(drop=True))
